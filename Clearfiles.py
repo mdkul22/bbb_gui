7e9525ca48a1b2990377d9e05ae525867ce95c8f
Mayunk <mdkul22@gmail.com>
2016-05-23 12:46:19 +0530

    changes

--- Clearfiles.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'MAHE'
-open('battery_voltage.txt', 'w').close()
-open('battery_current.txt', 'w').close()
-open('mppt_current1.txt', 'w').close()
-open('mppt_current2.txt', 'w').close()
-open('motor_voltage.txt', 'w').close()
-open('motor_current.txt', 'w').close()
-print('done')
